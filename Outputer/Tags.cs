461e93b74b83006c3e776f389202f9b014e59c12
Ivan Saez Zamora <ivsaez@gmail.com>
2024-01-19 22:34:28 +0100

    add tags

---
+++ Outputer/Tags.cs
@@ -0,0 +1,23 @@
+﻿namespace Outputer
+{
+    public class Tags
+    {
+        private readonly ISet<string> tags;
+
+        public Tags(params string[] tags)
+        {
+            this.tags = new HashSet<string>();
+            
+            foreach (var tag in tags)
+                Add(tag);
+        }
+
+        public void Add(string tag)
+        {
+            if(!string.IsNullOrWhiteSpace(tag)) 
+                tags.Add(tag);
+        }
+
+        public bool Has(string tag) => tags.Contains(tag);
+    }
+}
